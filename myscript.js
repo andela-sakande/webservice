921e0684c395a91ec88321fb27c5eef8147f5a4f
andela-sakande <surajudeen.akande@andela.com>
2016-05-23 08:58:13 +0100

    This would have made it dynamic.

--- myscript.js
+++ myscript.js
@@ -14,7 +14,7 @@ function loadInfo(){
 	var username = document.getElementById("username").value;
 	if(typeof username == "string" && username.length > 0){
 		$.ajax({
-			url: "https://api.github.com/users/mykel41?client_id=990e580361504aa076e0&client_secret=1234015a0cc5f49c8cf49456fa0c0018ef8ad1e0"
+			url: "https://api.github.com/users/"+username+"?client_id=990e580361504aa076e0&client_secret=1234015a0cc5f49c8cf49456fa0c0018ef8ad1e0"
 		}).then(function(data){
 			document.getElementById("img").src = data.avatar_url;
 			$('.id').append(data.id);
